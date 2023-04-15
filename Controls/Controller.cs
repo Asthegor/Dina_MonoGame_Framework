68aadf87029e2dcce88d111485f6256f6dc5daad
LACOMBE Dominique <lacombe.dominique@outlook.fr>
2023-04-15 16:07:29 -0400

    Update Controllers - Continuous action
    Add continuous action to controllers

--- Controls/Controller.cs
+++ Controls/Controller.cs
@@ -15,11 +15,18 @@ namespace Pong_DirectX.DinaFramework.Controls
     internal class KeyboardKey : IControllerKey
     {
         private Keys Key { get; set; }
+        private bool _continuous;
         private KeyboardState oldState = Keyboard.GetState();
-        public KeyboardKey(Keys key) { Key = key; }
+        public KeyboardKey(Keys key, bool continuous = false)
+        {
+            Key = key;
+            _continuous = continuous;
+        }
         public bool IsPressed()
         {
-            bool result = !oldState.IsKeyDown(Key) && Keyboard.GetState().IsKeyDown(Key);
+            bool result = Keyboard.GetState().IsKeyDown(Key);
+            if (!_continuous)
+                result = !oldState.IsKeyDown(Key) && Keyboard.GetState().IsKeyDown(Key);
             oldState = Keyboard.GetState();
             return result;
         }
@@ -28,11 +35,18 @@ namespace Pong_DirectX.DinaFramework.Controls
     internal class GamepadButton : IControllerKey
     {
         private Buttons Button { get; set; }
+        private bool _continuous;
         private GamePadState oldState = GamePad.GetState(PlayerIndex.One);
-        public GamepadButton(Buttons button) { Button = button; }
+        public GamepadButton(Buttons button, bool continuous = false)
+        {
+            Button = button;
+            _continuous = continuous;
+        }
         public bool IsPressed()
         {
-            bool result = !oldState.IsButtonDown(Button) && GamePad.GetState(PlayerIndex.One).IsButtonDown(Button);
+            bool result = GamePad.GetState(PlayerIndex.One).IsButtonDown(Button);
+            if(!_continuous)
+                result = !oldState.IsButtonDown(Button) && GamePad.GetState(PlayerIndex.One).IsButtonDown(Button);
             oldState = GamePad.GetState(PlayerIndex.One);
             return result;
         }
